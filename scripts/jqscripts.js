67b298533cf12aeca60478ae2ee62e8be51fb7d9
Traci Williams <ug3000@gmail.com>
2017-07-27 11:27:44 -0500

    sticky navigation

---
+++ scripts/jqscripts.js
@@ -0,0 +1,12 @@
+//simple test to make sure jquery works 
+//$(document).ready(function() {
+// 	$("h1").click(function(){
+// 		$(this).css("background-color","#ff0000");
+// 	})
+// });
+
+$(document).ready(function(){
+
+
+
+});
